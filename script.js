83f5d31f14ffd9a4a5fa0a076199765bb8e25009
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2022-12-16 16:40:27 +0000

    created script.js file and added comments

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1 - calculate total number of months in database using
+ 
+// 2 - calculate the net total amount of profits
+
+// 3 - calculate the net total amount of losses
+
+// 4 - average out the changes in profits 
+// using (`Total/Number of months`)
+
+// 5 - average out the changes in losses 
+// using (`Total/Number of months`)
+
+// 6 -The greatest increase in profits (date and amount)
+
+// 7 -greatest decrease in losses (date and amount)
+
