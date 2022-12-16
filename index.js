96592d35f44bb8369e1141c807044352913bac0f
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2022-12-16 16:47:38 +0000

    added total number of months, removed script.js

--- index.js
+++ index.js
@@ -86,3 +86,22 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+
+// 1 - calculate total number of months in database using
+
+console.log("Total number of months: ",finances.length)
+// 2 - calculate the net total amount of profits
+
+// 3 - calculate the net total amount of losses
+
+// 4 - average out the changes in profits 
+// using (`Total/Number of months`)
+
+// 5 - average out the changes in losses 
+// using (`Total/Number of months`)
+
+// 6 -The greatest increase in profits (date and amount)
+
+// 7 -greatest decrease in losses (date and amount)
+
